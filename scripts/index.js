87ed3177c3455593f0429fe0b28df8f0933c02f4
Dipali <savaliyadipali13@gmail.com>
2024-04-26 15:20:57 -0400

    array in JS
    Signed-off-by: Dipali <savaliyadipali13@gmail.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 //alert('I am alert inside script tag!')
 console.log('Hello Dipali');
 console.log(2+2);
+
+let students =[
+["a","b","c"],
+["d","e","f"]
+]
+
+for(let i=0; i<students[i].length;i++)
+{
+  console.log(students[i]);
+}
